2339b027ea0ae98e08b1a50de1a223d27b03b9b7
Sigfrido Gomez <Sigfrido@sigfridos-mbp.attlocal.net>
2019-02-20 00:13:32 -0600

    created tables and uploading csv

--- scripts/all_queries.py
+++ scripts/all_queries.py
@@ -5,6 +5,7 @@ a place to hold all my queries for this project
 #
 #inspection CSV files
 #completed 2/16
+
 inspection_data_create_table = '''CREATE TABLE osha_inspection_data
 	(
 	activity_nr int primary key,
@@ -47,37 +48,40 @@ inspection_data_create_table = '''CREATE TABLE osha_inspection_data
 
 #severe injury CSV
 # incident_id,event_date,employer,address1,address2,city,state,zipcode,latitude,longitude,naics_code,hospitalized,amputation,final_description,body_part
+##injury_index,incident_id,event_date,employer,address1,address2,city,state,zipcode,latitude,longitude,naics_code,hospitalized,amputation,final_description,body_part
 #completed 2/18
+#uploaded 2/19
 severe_injury_data_create_table = '''CREATE TABLE osha_severe_injury_data
 	(
-	incident_id int primary key,
+	injury_index int primary key,
+	incident_id BIGINT null,
 	event_date date null,
-	employer varchar(32 null,
+	employer varchar(128) null,
 	address1 varchar(128) null,
 	address2 varchar(128) null,
-	city char(20) null,
-	state char(2) null,
-	zipcode smallint null,
+	city char(64) null,
+	state char(32) null,
+	zipcode int null,
 	latitude float(32),
 	longitude float(32),
-	naics_code smallint,
-	hospitalized boolean,
-	amputation boolean,
-	final_description varchar(400),
-	body_part varchar(30)
+	naics_code int,
+	hospitalized smallint,
+	amputation smallint,
+	final_description varchar(3000),
+	body_part varchar(64)
 
 	);'''
 
 #fatalities CSV
 #date,company,city,state,zipcode
 #complete 2/18
-fatalities_data_data_create_table = '''CREATE TABLE osha_fatalities_data
+fatalities_data_create_table = '''CREATE TABLE osha_fatalities_data
 	(
 	incident_date date,
 	company  varchar(100) null, 
 	city varchar(50) null,
 	state char(2) null,
-	zipcode smallint null
+	zipcode int null
 	);'''
 
 #sic codes CSV
@@ -85,7 +89,7 @@ fatalities_data_data_create_table = '''CREATE TABLE osha_fatalities_data
 sic_codes_data_create_table = '''CREATE TABLE sic_codes_data
 	(
 	code smallint primary key,
-	description varchar(128),
+	description varchar(128)
 	);'''
 
 
@@ -93,15 +97,23 @@ sic_codes_data_create_table = '''CREATE TABLE sic_codes_data
 #nics codes CSV
 #codes,titles
 #completed2/178
-inspection_data_create_table = '''CREATE TABLE naics_codes_data
+#uploaded 2/19
+naics_codes_create_table = '''CREATE TABLE naics_codes_data
 	(
 	codes int primary key,
-	titles varchar(64) null
+	titles varchar(300) null
 	);'''
 
 
 
-
+ # public | naics_codes_data        | table | postgres
+ # public | osha_fatalities_data    | table | postgres
+ # public | osha_inspection_data    | table | postgres
+ 
+ # public | osha_severe_injury_data | table | postgres
+ 
+ # public | sic_codes_data          | table | postgres
+ # public | spatial_ref_sys         | table | postgres
 
 
 
--- scripts/loaddata.py
+++ scripts/loaddata.py
@@ -3,6 +3,7 @@ import os
 import psycopg2 as dbcooper
 import pprint
 import csv
+from all_queries import *
 #import pandas as pd
 #from config import config
 
@@ -50,94 +51,23 @@ def cooperTables(dbName, fName):
 	#will return object than can be used to query | cursor()
 	queryCon = conn.cursor()
 
-	#create spatial references for point data 
-	
-	#queryCon.execute("CREATE EXTENSION postgis")
-	
-	#ID,Case Number,Date,Time,Block,Primary Type,Description,Location Description,Arrest,Domestic,District,Ward,Community Area,FBI Code,Latitude,Longitude
-	# queryExpress = '''CREATE TABLE osha_inspection_data
-	# (
-	# activity_nr int primary key,
-	# reporting_id int null,
-	# state_flag varchar(6),
-	# estab_name varchar(128),
-	# site_address varchar(64),
-	# site_city varchar(64),
-	# site_state char(2),
-	# site_zip int,
-	# owner_type varchar(32),
-	# owner_code smallint null,
-	# adv_notice varchar(2),
-	# safety_hlth varchar(2),
-	# sic_code int null,
-	# naics_code int null,
-	# insp_type char(20),
-	# insp_scope char(20),
-	# why_no_insp char(32),
-	# union_status char(6),
-	# safety_manuf varchar(10),
-	# safety_const varchar(10),
-	# safety_marit varchar(10),
-	# health_manuf varchar(10),
-	# health_const varchar(10),
-	# health_marit varchar(10),
-	# migrant varchar(10),
-	# mail_street varchar(64),
-	# mail_city varchar(64),
-	# mail_state char(2),
-	# mail_zip int,
-	# host_est_key varchar(20),
-	# nr_in_estab int Null,
-	# open_date date Null,
-	# case_mod_date varchar(32) Null,
-	# close_conf_date varchar(32) Null,
-	# close_case_date varchar(32) Null,
-	# ld_dt TIMESTAMPTZ
-	# );'''
-	queryExpress = '''CREATE TABLE osha_inspection_data
-	(
-	activity_nr int primary key,
-	reporting_id int null,
-	state_flag varchar(6),
-	estab_name varchar(128),
-	site_address varchar(200),
-	site_city varchar(64),
-	site_state char(2),
-	site_zip int,
-	
-	owner_type varchar(32),
-	owner_code int null,
-	
-	adv_notice varchar(2),
-	safety_hlth varchar(2),
-	sic_code int null,
-	naics_code int null,
-	insp_type char(20),
-	insp_scope char(20),
-	why_no_insp char(32),
-	union_status char(6),
-	safety_manuf varchar(10),
-	safety_const varchar(10),
-	safety_marit varchar(10),
-	health_manuf varchar(10),
-	health_const varchar(10),
-	health_marit varchar(10),
-	migrant varchar(10),
-	mail_street varchar(200),
-	mail_city varchar(64),
-	mail_state char(2),
-	mail_zip int,
-	host_est_key varchar(20),
-	nr_in_estab int Null,
-	open_date date Null,
-	case_mod_date date Null,
-	close_conf_date date Null,
-	close_case_date date Null,
-	ld_dt TIMESTAMPTZ
-	);'''
-
 	#execute query 
-	queryCon.execute(queryExpress)
+	##inspection data
+	#queryCon.execute(inspection_data_create_table)
+	
+	##execute query 
+	##spatial data 
+	# queryCon.execute("CREATE EXTENSION postgis")
+	# queryCon.execute(severe_injury_data_create_table)
+	##execute query 
+	##fatalities
+	# queryCon.execute(fatalities_data_create_table)
+	# ##execute query 
+	# ##sic codes
+	queryCon.execute(sic_codes_data_create_table)
+	# ##execute query 
+	# ##fnaics
+	# queryCon.execute(naics_codes_create_table)		
 	#queryCon.commit()
 	print( "TABLE CREATED")
 	print("\n")
@@ -146,40 +76,6 @@ def cooperTables(dbName, fName):
 	conn.commit()
 	queryCon.close()
 
-
-# def cooperUpload(fName):
-# 	count = 0
-# 	connString = "host='localhost' dbname='%s' user='postgres' password='' " %(dbName)
-# 	# print the connection string we will use to connect
-# 	print("Connecting to database\n	->%s" % (connString))
-# 	#get a connection, exception will be raised here if no connection made
-# 	conn = dbcooper.connect(connString)
-# 	#will return object than can be used to query | cursor()
-# 	queryCon = conn.cursor()
-# 	print("\n")
-# 	print("\n")
-# 	#print fName
-# 	table_name = 'osha_inspection_data'
-# 	file_object = fName
-# 	SQL_STATEMENT = """
-#     COPY %s FROM STDIN WITH
-#     DELIMITER AS ','
-#     QUOTE '"'
-#     HEADER
-#     CSV    
-#     """
-#     while queryCon.copy_expert(sql=SQL_STATEMENT % table_name, file=file_object) == True:
-#     	try:
-#     		queryCon.copy_expert(sql=SQL_STATEMENT % table_name, file=file_object)
-# 			conn.commit()
-# 			queryCon.close()
-# 			print("UPLOAD SUCCESS!!!!")
-# 			print("\n")
-# 			print("\n")
-# 		except Exception:
-# 			count +=1
-# 			print(count)
-# 			pass
 
 
 def cooperUpload(fName):
@@ -193,7 +89,7 @@ def cooperUpload(fName):
 	print("\n")
 	print("\n")
 	#print fName
-	table_name = 'osha_inspection_data'
+	table_name = 'sic_codes_data'
 	file_object = fName
 	SQL_STATEMENT = """
     COPY %s FROM STDIN WITH
@@ -213,8 +109,8 @@ def cooperUpload(fName):
 
 
 
-
-
+# dbName = 'osha_database'
+# cooperTables(dbName)
 
 ##################
 #Global Variable 
@@ -244,13 +140,13 @@ with open(csv_file, 'rb') as f:
 	dbName = 'osha_database'
 
 
-	# # create DB
+	## create DB
 	# cooperCreate(dbName)
 
-	# # creates tables 
+	##creates tables 
 	# cooperTables(dbName,f)
 
-	#upload csv
+	##upload csv
 	cooperUpload(f)
 
-#	f.close()
+	f.close()
